84cc4448dfcc788df340705e1fa0fa539ef5ce12
naagss <nagashreel15@gmail.com>
2024-07-09 01:02:34 +0530

    2nd Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Java
+
+Author - Nagashree L
+Date : 09.07.2024
+This Repo contains my practice
+
+- core java
+- OOPS
+- Collection Framework
+- Multithreading
+- Design Pattern
+- File io
--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,2 +1,16 @@
-package ex_07072024;public class Lab001 {
+package ex_07072024;
+
+public class Lab001 {
+
+    public static void main(String[] args)
+    {
+        int a=10;
+        System.out.println("Hello World, Nagashree Here!!");
+        System.out.println(a);
+        System.out.println(2+2);
+        System.out.println(123);
+
+        //System.out.println(10/0);
+
+    }
 }
